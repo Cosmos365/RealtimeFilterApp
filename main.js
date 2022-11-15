d83944a8072a5e089eed64b1e3160d64a7f3f4c0
Avi <81462673+Cosmos365@users.noreply.github.com>
2022-11-15 18:04:33 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+nosex = 0;
+nosey = 0;
 
+function preload() {
+    elephantnose = loadImage('https://i.postimg.cc/HnWjp9Nb/23-238215-cartoon-elephant-trunk-clip-art-clip-art.png');
 }
 
 function setup() {
@@ -15,10 +18,12 @@ function setup() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose x = "+results[0].pose.nose.x);
-        console.log("nose y = "+results[0].pose.nose.y);
-    };
-};
+        nosex = results[0].pose.nose.x-5;
+        nosey = results[0].pose.nose.y-5;
+        console.log("nose x = "+nosex);
+        console.log("nose y = "+nosey);
+    };  
+}
 
 function modelLoaded() {
     console.log('model loaded');
@@ -26,6 +31,10 @@ function modelLoaded() {
 
 function draw() {
     image(video,0,0,300,300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    //circle(nosex,nosey,20);
+    image(elephantnose,nosex,nosey,30,30);
 }
 
 function take_snapshot() {
